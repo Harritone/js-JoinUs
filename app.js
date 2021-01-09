455cb1e2c197302a258c8dca35dc72de1b291825
Harritone <yourrasik@gmail.com>
2021-01-09 12:14:50 +1000

    Inserted bunch of data

--- app.js
+++ app.js
@@ -9,11 +9,26 @@ const connection = mysql.createConnection({
   database: 'join_us',
 });
 
-const q = 'SELECT CURTIME() AS time, CURDATE() as date, NOW() as now';
-connection.query(q, (error, result, fields) => {
-  if (error) throw error;
-  console.log(result[0].time);
-  console.log(result[0].date);
-  console.log(result[0].now);
+// Selecting DATA
+// const q = 'SELECT COUNT(*) AS total FROM users';
+
+// connection.query(q, (error, result, fields) => {
+//   if (error) throw error;
+//   console.log(result[0].total);
+// });
+
+// Inserting data
+
+const users = [];
+
+for (let i = 0; i < 500; i++) {
+  users.push([faker.internet.email(), faker.date.past()]);
+}
+console.log(users.length);
+const q = 'INSERT INTO users(email, created_at) VALUES ?';
+
+connection.query(q, [users], (error, result) => {
+  console.log(error);
+  console.log(result);
 });
 connection.end();
